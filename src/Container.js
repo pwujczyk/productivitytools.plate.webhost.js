d7aa40ba6ac97ea954a8b1028934fb8263012ed3
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-17 09:57:20 +0200

    add next version

--- src/Container.js
+++ src/Container.js
@@ -1,9 +1,20 @@
 import { PTPlate } from "productivitytools.plate";
 
+const initialValue = (content) => [
+    {
+      type: "p",
+      children: [
+        {
+          text: content,
+        },
+      ],
+    } 
+  ];
+
 function Container(){
     return(<div>
         <span>Container</span>
-        <PTPlate content="pawel"></PTPlate>
+        <PTPlate content={initialValue("pawel")}></PTPlate>
         <span>ptplate ended</span>
     </div>)
 }
